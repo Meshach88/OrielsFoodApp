11e6564e7e158688031f3bce67af35d6cd55ac79
Meshach <116318961+Meshach88@users.noreply.github.com>
2024-10-11 06:51:06 +0100

    files update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing user order from frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5174";
+  const frontend_url = "https://oriels.onrender.com";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
